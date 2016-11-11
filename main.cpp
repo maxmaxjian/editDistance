01ae3702055960c199c5c7ec5ceb80925cdef8eb
Jian Wei <jianwei@jianwei-deb8-64>
2016-11-11 16:34:43 -0500

    first edit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class solution {
+  public:
+    int minDistance(const std::string & word1, const std::string & word2) {
+        
+    }
+};
+
+int main() {
+    
+}
